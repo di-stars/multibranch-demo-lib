8a02c7bae0edf9e09e0e0cef1f6954151640f0cc
di-stars <di.astreya@gmail.com>
2020-06-24 16:37:07 +0300

    Create zalupa.groovy

---
+++ vars/zalupa.groovy
@@ -0,0 +1,41 @@
+import com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition
+
+def checkBox (String name, String values, String defaultValue,
+              int visibleItemCnt=0, String description='', String delimiter=',') {
+
+    // default same as number of values
+    visibleItemCnt = visibleItemCnt ?: values.split(',').size()
+    return new ExtendedChoiceParameterDefinition(
+            name, //name,
+            "PT_CHECKBOX", //type
+            values, //value
+            "", //projectName
+            "", //propertyFile
+            "", //groovyScript
+            "", //groovyScriptFile
+            "", //bindings
+            "", //groovyClasspath
+            "", //propertyKey
+            defaultValue, //defaultValue
+            "", //defaultPropertyFile
+            "", //defaultGroovyScript
+            "", //defaultGroovyScriptFile
+            "", //defaultBindings
+            "", //defaultGroovyClasspath
+            "", //defaultPropertyKey
+            "", //descriptionPropertyValue
+            "", //descriptionPropertyFile
+            "", //descriptionGroovyScript
+            "", //descriptionGroovyScriptFile
+            "", //descriptionBindings
+            "", //descriptionGroovyClasspath
+            "", //descriptionPropertyKey
+            "", //javascriptFile
+            "", //javascript
+            false, //saveJSONParameterToFile
+            false, //quoteValue
+            visibleItemCnt, //visibleItemCount
+            description, //description
+            delimiter //multiSelectDelimiter
+            )
+}
